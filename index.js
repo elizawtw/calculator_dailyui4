090400f7212d56513a309264386a90f4a5e49cd1
eliza <twew02@gmail.com>
2022-10-20 19:28:03 -0400

    work on css for calculator and add javascript file

---
+++ index.js
@@ -0,0 +1,6 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation');
+
+const deleteButton = document.querySelector('[data-delete]');
+
+const allClearButton = document.querySelector('[data-all-clear');
